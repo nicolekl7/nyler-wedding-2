7062af2d63932b166ea0642222b8fed76d1bbbf4
nicolekl7 <nicole.landmesser@aol.com>
2026-03-24 22:34:25 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ const App = () => (
     <TooltipProvider>
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/the-weekend" element={<TheWeekend />} />
